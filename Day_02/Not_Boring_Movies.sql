f03d92175d11cd4c41445dc211f4fb034cccacb9
ROVIN CHAUHAN <rovinchauhan.97@gmail.com>
2026-06-16 23:09:28 +0530

    Add query for non-boring movies from cinema
    Add SQL query to select non-boring movies with odd IDs ordered by rating.

--- Day_02/Not_Boring_Movies.sql
+++ Day_02/Not_Boring_Movies.sql
@@ -1,1 +1,8 @@
+-- Problem: Not Boring Movies
+-- Difficulty: Easy
+-- Concepts: SELECT, WHERE, DESCENDING
 
+SELECT *
+FROM cinema
+WHERE id % 2 =1 AND description != 'boring'
+ORDER BY rating DESC

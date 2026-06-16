df515c5eb3fcc9ee54cf78d7b2907a9569f25301
ROVIN CHAUHAN <rovinchauhan.97@gmail.com>
2026-06-16 22:51:29 +0530

    Enhance SQL query with ORDER BY and DISTINCT
    Added ORDER BY and DISTINCT to the SQL query.

--- Day_02/Article_Views_I.sql
+++ Day_02/Article_Views_I.sql
@@ -1,5 +1,9 @@
 -- Problem: Article Views I
 -- Difficulty: Easy
--- Concepts: SELECT, WHERE
+-- Concepts: SELECT, WHERE, ORDER BY, DISTINCT
 
+SELECT DISTINCT author_id as id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY id ASC;
 

adaa616bbb3b5a2b1af71260734b387bf7086ff2
ROVIN CHAUHAN <rovinchauhan.97@gmail.com>
2026-06-30 17:07:10 +0530

    Revise problem statement and SQL query concepts
    Updated problem statement and added concepts for clarity.

---
+++ 11.Average_Time_of_Process_per_Machine.sql
@@ -0,0 +1,17 @@
+-- Problem: Average Time of Process per Machine
+-- Difficulty: Easy
+-- Concepts: SELECT, WHERE, SELF JOIN, ROUND, AVG
+
+SELECT 
+    s.machine_id, 
+    ROUND(AVG(e.timestamp-s.timestamp),3) as processing_time 
+FROM 
+    Activity s
+JOIN 
+    Activity e 
+ON 
+    e.machine_id=s.machine_id
+WHERE
+    s.activity_type='start' AND e.activity_type='end'
+GROUP BY 
+    s.machine_id
